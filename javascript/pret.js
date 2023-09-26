817e92ac73e56289fc400648d6b9a68ab08c73fe
iMac_03 <imac_03@192.168.50.18>
2023-09-26 17:17:43 +0000

    very

---
+++ javascript/pret.js
@@ -0,0 +1,21 @@
+let formulaire = document.querySelector(".formulaire");
+formulaire.addEventListener("submit",event=>{
+    event.preventDefault()
+    const form = event.target;
+    const categorie = form.categorie;
+    const montant =form.montant;
+    const duree = form.duree;
+    const gobaldata = localStorage.PRETS ? JSON.parse(localStorage.PRETS) : [];
+    const objet = {
+        categorie : categorie.value,
+        montant : montant.value,
+        duree : duree.value
+    };
+
+    gobaldata.push(objet);
+    localStorage.setItem("PRETS",JSON.stringify(gobaldata));
+    alert(" requet effectue avec succes")
+    categorie.value="";
+    montant.value="";
+    duree.value="";
+})
